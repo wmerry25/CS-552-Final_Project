5ef59cbcbc96b46be99d8f83e75c88a684522a23
wmerry25 <114078333+wmerry25@users.noreply.github.com>
2026-03-28 19:10:45 -0500

    Base bot

--- interface.py
+++ interface.py
@@ -1,43 +1,42 @@
 import streamlit as st
-import random
-import time
+import replicate
+import os
 
-
-# Streamed response emulator
-def response_generator():
-    response = random.choice(
-        [
-            "Hello there! How can I assist you today?",
-            "Hi, human! Is there anything I can help you with?",
-            "Do you need help?",
-        ]
-    )
-    for word in response.split():
-        yield word + " "
-        time.sleep(0.05)
-
-
-st.title("Simple chat")
-
-# Initialize chat history
+replicate_api = st.secrets['REPLICATE_API_TOKEN']
 if "messages" not in st.session_state:
-    st.session_state.messages = []
+    st.session_state["messages"] = [{"role": "assistant", "content": "How can I help you?"}]
 
-# Display chat messages from history on app rerun
-for message in st.session_state.messages:
-    with st.chat_message(message["role"]):
-        st.markdown(message["content"])
+for msg in st.session_state.messages:
+    st.chat_message(msg["role"]).write(msg["content"])
 
-# Accept user input
-if prompt := st.chat_input("What is up?"):
-    # Add user message to chat history
+def generate_response_with_context(prompt_input):
+    history = ""
+    base_prompt = "You are an expert on coral reef systems in the home. Below is the history of this dialogue, only respond once as 'Assistant'."
+    for hist_message in st.session_state.messages:
+        if hist_message["role"] == "user":
+            history += "User: " + hist_message["content"] + "\n\n"
+        else:
+            history += "Assistant: " + hist_message["content"] + "\n\n"
+    return replicate.run(
+        'meta/llama-2-7b-chat',
+        input={"prompt": f"{history} {prompt_input} Assistant: ",
+               "max_length": 500, "repetition_penalty": 1}
+    )
+
+if prompt := st.chat_input():
+    if 'REPLICATE_API_TOKEN' not in st.secrets:
+        st.info("Please add your Replicate API key to continue.")
+        st.stop()
+         
     st.session_state.messages.append({"role": "user", "content": prompt})
-    # Display user message in chat message container
-    with st.chat_message("user"):
-        st.markdown(prompt)
-
-    # Display assistant response in chat message container
-    with st.chat_message("assistant"):
-        response = st.write_stream(response_generator())
-    # Add assistant response to chat history
-    st.session_state.messages.append({"role": "assistant", "content": response})
+    st.chat_message("user").write(prompt)
+    if st.session_state.messages[-1]["role"] != "assistant":
+        with st.chat_message("assistant"):
+            with st.spinner("Thinking..."):
+                response = generate_response_with_context(prompt)
+                em = st.empty()
+                message = ''
+                for i in response:
+                    message += em
+                    em.markdown(message)
+                st.session_state.messages.append({"role": "assistant", "content": message})
