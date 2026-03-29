fb1be248c26837cd4c0fe234906c92f25e1270f4
wmerry25 <114078333+wmerry25@users.noreply.github.com>
2026-03-28 20:27:17 -0500

    Bug fix to create one stream of text

--- interface.py
+++ interface.py
@@ -23,19 +23,19 @@ if prompt := st.chat_input():
                 for h in st.session_state.messages[:-1]:
                     history += f"{h['role']}: {h['content']}\n\n"
 
-                    stream = replicate.stream(
-                        "meta/meta-llama-3-8b-instruct",
-                        input={
-                            "top_k": 0,
-                            "top_p": 0.95,
-                            "prompt": prompt,
-                            "system_prompt": "You are an expert on coral reef systems in the home. Use the following history to contextulize your answer: " + history,
-                            "length_penalty": 1,
-                            "max_new_tokens": 512,
-                            "stop_sequences": "<|end_of_text|>,<|eot_id|>",
-                            "prompt_template": "<|begin_of_text|><|start_header_id|>system<|end_header_id|>\n\n{system_prompt}<|eot_id|><|start_header_id|>user<|end_header_id|>\n\n{prompt}<|eot_id|><|start_header_id|>assistant<|end_header_id|>\n\n",
-                            "presence_penalty": 0,
-                            "log_performance_metrics": False
-                        })
+                stream = replicate.stream(
+                    "meta/meta-llama-3-8b-instruct",
+                    input={
+                        "top_k": 0,
+                        "top_p": 0.95,
+                        "prompt": prompt,
+                        "system_prompt": "You are an expert on coral reef systems in the home. Keep your answers short, especially if the user is not asking a complex question. Use the following history to contextulize your answer: " + history,
+                        "length_penalty": 1,
+                        "max_new_tokens": 512,
+                        "stop_sequences": "<|end_of_text|>,<|eot_id|>",
+                        "prompt_template": "<|begin_of_text|><|start_header_id|>system<|end_header_id|>\n\n{system_prompt}<|eot_id|><|start_header_id|>user<|end_header_id|>\n\n{prompt}<|eot_id|><|start_header_id|>assistant<|end_header_id|>\n\n",
+                        "presence_penalty": 0,
+                        "log_performance_metrics": False
+                    })
                 response = st.write_stream(stream)
                 st.session_state.messages.append({"role": "assistant", "content": response})
