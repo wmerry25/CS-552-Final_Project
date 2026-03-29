c6c5a4f9cb998b4ba6ade18f9f77f0df3c0c7d47
wmerry25 <114078333+wmerry25@users.noreply.github.com>
2026-03-28 20:08:10 -0500

    Updates using context from replicate

--- interface.py
+++ interface.py
@@ -9,20 +9,6 @@ if "messages" not in st.session_state:
 for msg in st.session_state.messages:
     st.chat_message(msg["role"]).write(msg["content"])
 
-def generate_response_with_context(prompt_input):
-    history = ""
-    base_prompt = "You are an expert on coral reef systems in the home. Below is the history of this dialogue, only respond once as 'Assistant'."
-    for hist_message in st.session_state.messages:
-        if hist_message["role"] == "user":
-            history += "User: " + hist_message["content"] + "\n\n"
-        else:
-            history += "Assistant: " + hist_message["content"] + "\n\n"
-    return replicate.run(
-        'meta/llama-2-7b-chat',
-        input={"prompt": f"{history} {prompt_input} Assistant: ",
-               "max_length": 500, "repetition_penalty": 1}
-    )
-
 if prompt := st.chat_input():
     if 'REPLICATE_API_TOKEN' not in st.secrets:
         st.info("Please add your Replicate API key to continue.")
@@ -33,10 +19,24 @@ if prompt := st.chat_input():
     if st.session_state.messages[-1]["role"] != "assistant":
         with st.chat_message("assistant"):
             with st.spinner("Thinking..."):
-                response = generate_response_with_context(prompt)
-                em = st.empty()
-                message = ''
-                for i in response:
-                    message += em
-                    em.markdown(message)
-                st.session_state.messages.append({"role": "assistant", "content": message})
+                history = ''
+                for h in st.session_state.messages[:-1]:
+                    history += f"{h['role']}: {h['content']}\n\n"
+                stream = replicate.stream(
+                    "meta/llama-2-7b-chat",
+                    input={
+                        "top_k": 0,
+                        "top_p": 1,
+                        "prompt": prompt,
+                        "max_tokens": 512,
+                        "temperature": 0.75,
+                        "system_prompt": "You are an expert on coral reef systems in the home. Use the following history to contextulize your answer: " + history,
+                        "length_penalty": 1,
+                        "max_new_tokens": 800,
+                        "prompt_template": "<s>[INST] <<SYS>>\n{system_prompt}\n<</SYS>>\n\n{prompt} [/INST]",
+                        "presence_penalty": 0,
+                        "log_performance_metrics": False
+                    },
+                )
+                response = st.write_stream(stream)
+                st.session_state.messages.append({"role": "assistant", "content": response})
